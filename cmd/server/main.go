fd1f74df882beef0978778b54c3797092bf56b9a
BennerG <bgurrola.btg@gmail.com>
2026-06-16 09:11:00 -0700

    chore: Add Chi logging middleware

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -80,6 +80,7 @@ func run() error {
 	r := chi.NewRouter()
 	r.Use(chimiddleware.RequestID)
 	r.Use(chimiddleware.RealIP)
+	r.Use(chimiddleware.Logger)
 	r.Use(chimiddleware.Recoverer)
 
 	// API handlers
